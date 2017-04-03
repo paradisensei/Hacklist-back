de4b819bebac06a886b7b8926e4057b57971100d
Neil Alishev <alishev.neil@gmail.com>
2017-04-03 20:13:35 +0300

    parse date and overall refactoring

--- src/main/java/org/hacklist/parser/impl/ItEventsParser.java
+++ src/main/java/org/hacklist/parser/impl/ItEventsParser.java
@@ -12,101 +12,217 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.Locale;
 
 /**
  * @author Aidar Shaifutdinov.
  */
 @Component
 public class ItEventsParser implements Parser {
+    private static final String WEBSITE = "http://it-events.com";
+    private static final String STUB_IMAGE = "https://i.ytimg.com/vi/tNJQY1tP5mY/maxresdefault.jpg";
 
-    @Override
-    public Collection<Hack> parse() throws IOException {
-        String website = "http://it-events.com";
-        Document doc = Jsoup.connect(website).get();
-        // event blocks on main page
-        Elements events = doc.getElementsByClass("vevent");
-        Collection<Hack> hacks = new LinkedList<>();
-        // stub url
-        String stubUrl = "http://s.quickmeme.com/img/1f/1fbf5db595577db3330029140dd253acca1e874e5fb8eb50e3876417f1061870.jpg";
-        for (Element e : events) {
-            String link = e.select("a.summary").attr("href");
-            // load event page
-            doc = Jsoup.connect(website + link).get();
-            Hack hack = new Hack();
-            // get registration url
-            Elements boxReg = doc.getElementsByClass("box_register");
-            if (!CollectionUtils.isEmpty(boxReg)) {
-                Elements regLink = boxReg.first().select("a");
-                if (!CollectionUtils.isEmpty(regLink)) {
-                    String url = regLink.attr("href");
-                    if (url.length() < 250) {
-                        hack.setUrl(url);
-                    } else {
-                        hack.setUrl(stubUrl);
-                    }
-                } else {
-                    hack.setUrl(stubUrl);
-                }
-            } else {
-                hack.setUrl(stubUrl);
-            }
-            // get event title
-            String title = doc.select(".vevent .caption").first().text();
-            hack.setTitle(title);
-            // get event organizer
-            Elements org = doc.select(".organizer span");
-            if (!CollectionUtils.isEmpty(org)) {
-                hack.setOrganizer(org.first().text());
-            } else {
-                continue;
-            }
-            // get event city and address
-            Elements adrs = doc.select(".adr");
-            if (!CollectionUtils.isEmpty(adrs)) {
-                Element adr = adrs.first();
-                Elements reg = adr.select(".region");
-                if (!CollectionUtils.isEmpty(reg)) {
-                    City city = City.getCity(reg.first().text().split(" , ")[0]);
-                    if (city == null) {
-                        continue;
-                    }
-                    hack.setCity(city);
-                } else {
-                    continue;
-                }
-                Elements str = adr.select(".street-adress");
-                if (!CollectionUtils.isEmpty(str)) {
-                    hack.setAddress(str.first().text());
-                } else {
-                    continue;
-                }
-            } else {
-                continue;
-            }
-            // get event description
-            Element anons = doc.select(".anons").first();
-            String description = anons.select("p").first().text();
-            hack.setDescription(description);
-            // get event image
-            Elements img = anons.select("img");
-            if (!CollectionUtils.isEmpty(img)) {
-                String imageUrl = website + img.first().attr("src");
-                if (imageUrl.length() < 250) {
-                    hack.setImageUrl(website + img.first().attr("src"));
-                } else {
-                    hack.setImageUrl(stubUrl);
-                }
-            } else {
-                hack.setImageUrl(stubUrl);
-            }
-            hack.setDate(new Date());
-            hack.setCategory(Category.COMMON);
-            hacks.add(hack);
+    private Document mainPage;
+    private Elements events;
+    private Collection<Hack> result;
+
+    public ItEventsParser() {
+        try {
+            mainPage = Jsoup.connect(WEBSITE).get();
+        } catch (IOException ignored) {
         }
-        return hacks;
+        // event blocks on the main page
+        events = mainPage.getElementsByClass("vevent");
+        result = new LinkedList<>();
     }
 
+    @Override
+    public Collection<Hack> parse() {
+        for (Element eventBlock : events) {
+            Document eventPage = goToEventPage(eventBlock);
+
+            if (eventPage == null)
+                continue;
+
+            String eventRegistrationUrl = getRegistrationUrl(eventPage);
+
+            if (eventRegistrationUrl == null)
+                continue;
+
+            String eventTitle = getEventTitle(eventPage);
+
+            if (eventTitle == null)
+                continue;
+
+            String eventOrganizer = getEventOrganizer(eventPage);
+
+            if (eventOrganizer == null)
+                continue;
+
+            // get event city and address
+            Elements adrs = eventPage.select(".adr");
+            if (CollectionUtils.isEmpty(adrs))
+                continue;
+
+            City eventCity = getEventCity(adrs);
+
+            if (eventCity == null)
+                continue;
+
+            String eventAddress = getEventAddress(adrs);
+
+            if (eventAddress == null)
+                continue;
+
+            String eventDescription = getEventDescription(eventPage);
+
+            String eventImage = getEventImage(eventPage);
+
+            Date eventDate = getEventDate(eventPage);
+
+            if (eventDate == null)
+                continue;
+
+            // hack construction
+            Hack hack = new Hack();
+            hack.setUrl(eventRegistrationUrl);
+            hack.setTitle(eventTitle);
+            hack.setOrganizer(eventOrganizer);
+            hack.setCity(eventCity);
+            hack.setAddress(eventAddress);
+            hack.setDescription(eventDescription);
+            hack.setImageUrl(eventImage);
+            hack.setDate(eventDate);
+            hack.setCategory(Category.COMMON);
+
+            result.add(hack);
+        }
+        return result;
+    }
+
+    private Document goToEventPage(Element eventBlock) {
+        String link = eventBlock.select("a.summary").attr("href");
+        Document eventPage = null;
+
+        // load event page
+        try {
+            eventPage = Jsoup.connect(WEBSITE + link).get();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        return eventPage;
+    }
+
+    private String getRegistrationUrl(Document eventPage) {
+        Elements boxReg = eventPage.getElementsByClass("box_register");
+        if (!CollectionUtils.isEmpty(boxReg)) {
+            Elements regLink = boxReg.first().select("a");
+            if (!CollectionUtils.isEmpty(regLink)) {
+                String url = regLink.attr("href");
+                if (url.length() < 250) {
+                    return url;
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    private String getEventTitle(Document eventPage) {
+        String eventTitle = eventPage.select(".vevent .caption").first().text();
+
+        if (eventTitle.length() > 150)
+            return null;
+
+        return eventTitle;
+    }
+
+    private String getEventOrganizer(Document eventPage) {
+        Elements org = eventPage.select(".organizer span");
+
+        if (CollectionUtils.isEmpty(org))
+            return null;
+
+        String organizer = org.first().text();
+        if (organizer.length() > 100)
+            return null;
+
+        return organizer;
+    }
+
+    private City getEventCity(Elements adrs) {
+        Element adr = adrs.first();
+        Elements reg = adr.select(".region");
+
+        if (CollectionUtils.isEmpty(reg))
+            return null;
+
+        City city = City.getCity(reg.first().text().split(" , ")[0]);
+        if (city == null)
+            return null;
+
+        return city;
+    }
+
+    private String getEventAddress(Elements adrs) {
+        Element adr = adrs.first();
+        Elements str = adr.select(".street-adress");
+        if (CollectionUtils.isEmpty(str))
+            return null;
+
+        String address = str.first().text();
+        if (address.length() > 100)
+            return null;
+
+        return address;
+    }
+
+    private String getEventDescription(Document eventPage) {
+        Element anons = eventPage.select(".anons").first();
+        return anons.select("p").first().text();
+    }
+
+    private String getEventImage(Document eventPage) {
+        Element anons = eventPage.select(".anons").first();
+        Elements img = anons.select("img");
+
+        if (CollectionUtils.isEmpty(img))
+            return STUB_IMAGE;
+
+        String imageUrl = WEBSITE + img.first().attr("src");
+
+        if (imageUrl.length() >= 250)
+            return STUB_IMAGE;
+
+        return WEBSITE + img.first().attr("src");
+    }
+
+    private Date getEventDate(Document eventPage) {
+        String eventDate = eventPage
+                .select("#event_end_date")
+                .first()
+                .text()
+                .replaceAll("\\(.+\\), ", "");
+
+        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy HH:mm", new Locale("ru"));
+        Date date = null;
+
+        try {
+            date = dateFormat.parse(eventDate);
+        } catch (ParseException ignored) {
+        }
+
+        return date;
+    }
 }
